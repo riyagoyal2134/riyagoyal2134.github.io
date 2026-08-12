6d58b15644c1e8e3bad0b784636b978ed9062eaa
riyagoyal2134 <rg85@njit.edu>
2026-08-12 00:29:37 -0400

    update home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 export default function Home() {
   return (
     <main>
-        <p>As a PhD student in Applied Mathematics, CS & Math Honors Graduate , I’ve led research in AI, complex networks, and healthcare technology. My mission is to bridge theory and innovation, creating ethical, scalable solutions that drive global change.</p>
-        <br></br>
-        <p>My journey blends rigorous academic achievement with hands-on impact. Graduating Summa Cum Laude with two B.S. degrees, three minors, and honors/program distinctions, I’ve designed graph algorithms, developed AI-driven tools for healthcare, and led interdisciplinary teams to tackle complex challenges.</p>
-        <br></br>
-        <p>As I begin my PhD, I am excited to explore new frontiers-particularly in AI, optimization, FinTech, and complex systems-and build robust, first-principles models in discrete-time frameworks for portfolio optimization that create solutions with lasting societal impact.</p><br></br>
-
+       <section>
+          <p>As a second-year PhD student in Applied Mathematics and a CS & Math Honors Graduate, I have led research in AI, complex networks, and healthcare technology. My mission is to bridge theory and innovation, creating ethical, scalable solutions that drive global change.</p>
+          <br />
+          <p>My journey blends rigorous academic achievement with hands-on impact. Graduating Summa Cum Laude with two B.S. degrees, three minors, and honors/program distinctions, I’ve designed graph algorithms, developed AI-driven tools for healthcare, and led interdisciplinary teams to tackle complex challenges.</p>
+          <br />
+          <p>In my PhD studies, I am excited to explore new frontiers-particularly in AI, optimization, FinTech, Numerical PDEs, and complex systems, and build robust solutions that have a lasting societal impact.</p>
+      </section>
         <section id="upcoming">
             <h2>Upcoming Presentations</h2>
             <ul>
